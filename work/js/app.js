e910f0e8625903edb547d4a310fe061b9b9ea7fa
tetsu-takumi <detsu.0113.soccer@icloud.com>
2025-08-02 17:36:45 +0900

    作成完了

--- work/js/app.js
+++ work/js/app.js
@@ -14,6 +14,16 @@ document.getElementById("get-weather").addEventListener("click", () => {
     })
     .then(weather => {
         console.log(weather);
-
+        let area = weather[0].timeSeries[0].areas[0];
+        let tempsArea = weather[1].tempAverage.areas[0];
+        console.log(tempsArea);
+        document.getElementById("publishingOffice").lastElementChild.textContent = weather[0].publishingOffice;
+        document.getElementById("reportDatetime").lastElementChild.textContent = weather[0].reportDatetime;
+        document.getElementById("targetArea").lastElementChild.textContent = area.area.name;
+        document.getElementById("todayHighTemperature").lastElementChild.textContent = tempsArea.max + "℃";
+        document.getElementById("todayLowTemperature").lastElementChild.textContent = tempsArea.min + "℃";
+        document.getElementById("today").lastElementChild.textContent = area.weathers[0];
+        document.getElementById("tomorrow").lastElementChild.textContent = area.weathers[1];
+        document.getElementById("dayAfterTomorrow").lastElementChild.textContent = area.weathers[2];
     })
 });
